7214c77f59075c9de6872494341db1e1da0b1c90
Fued Kraide <omekra@hotmail.com>
2023-06-30 19:57:35 +1000

    style: formatted login page

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,58 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import Styles from './login-styles.scss'
-import { LoginHeader, FormStatus, Footer, Input, } from '@/presentation/components'
-import Context from '@/presentation/contexts/form/form-context'
-import { Validation } from '@/presentation/protocols/validation'
-import { Authentication } from '@/domain/usecases'
+import React, { useState, useEffect } from "react";
+import Styles from "./login-styles.scss";
+import {
+  LoginHeader,
+  FormStatus,
+  Footer,
+  Input,
+} from "@/presentation/components";
+import Context from "@/presentation/contexts/form/form-context";
+import { Validation } from "@/presentation/protocols/validation";
+import { Authentication } from "@/domain/usecases";
 
 type Props = {
-  validation: Validation
-  authentication: Authentication
-}
+  validation: Validation;
+  authentication: Authentication;
+};
 
 const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
-    email: '',
-    password: '',
-    emailError: '',
-    passwordError: '',
-    mainError: ''
-  })
+    email: "",
+    password: "",
+    emailError: "",
+    passwordError: "",
+    mainError: "",
+  });
 
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate('email', state.email ),
-      passwordError: validation.validate('password', state.password )
-    })
-  }, [state.email, state.password])
+      emailError: validation.validate("email", state.email),
+      passwordError: validation.validate("password", state.password),
+    });
+  }, [state.email, state.password]);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    event.preventDefault()
-    setState({ ...state, isLoading: true })
-    await authentication.auth({ 
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+    setState({ ...state, isLoading: true });
+    await authentication.auth({
       email: state.email,
-      password: state.password
-    })
-  }
+      password: state.password,
+    });
+  };
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form} onSubmit={handleSubmit} >
+        <form className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
-          <Input type='email' name="email" placeholder='Type your email' />
-          <Input type="password" name="password" placeholder='Type your password' />
-          <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type="submit">Login</button>
+          <Input type="email" name="email" placeholder="Type your email" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Type your password"
+          />
+          <button
+            data-testid="submit"
+            disabled={!!state.emailError || !!state.passwordError}
+            className={Styles.submit}
+            type="submit"
+          >
+            Login
+          </button>
           <span className={Styles.link}>Register</span>
-        <FormStatus />
+          <FormStatus />
         </form>
       </Context.Provider>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
